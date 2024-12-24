c3c703f1e077c95378034bc0c33f93a652fe9112
Guillem Barta <97833203+Willy8m@users.noreply.github.com>
2024-12-24 10:42:02 +0100

    pre-Azure devops commit

--- app/src/main/java/com/example/wakeworddisplayimage/OpenWakeWord.kt
+++ app/src/main/java/com/example/wakeworddisplayimage/OpenWakeWord.kt
@@ -1,5 +1,6 @@
 package com.example.wakeworddisplayimage
 
+import ai.onnxruntime.OnnxSequence
 import ai.onnxruntime.OnnxTensor
 import ai.onnxruntime.OrtEnvironment
 import ai.onnxruntime.OrtSession
@@ -14,7 +15,7 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
-import com.example.wakeworddisplayimage.ml.Alexa20000100001200
+import com.example.wakeworddisplayimage.ml.AlexaCa2500015000100
 import com.example.wakeworddisplayimage.ml.EmbeddingModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -31,7 +32,7 @@ import java.util.LinkedList
 class OpenWakeWord (private val context: MainActivity, private val viewModel: MainViewModel) {
 
     // Configuration
-    private val gain = 3
+    private val gain = 100
     private var maxPatience = 20
     private val audioBufferSizeInBytes = 1280 * 4
     private val maxScores = 1
@@ -39,7 +40,8 @@ class OpenWakeWord (private val context: MainActivity, private val viewModel: Ma
     // Models
     private lateinit var melspecOnnx: OrtSession
     private lateinit var embeddingModel : EmbeddingModel
-    private lateinit var wakewordModel : Alexa20000100001200
+    private lateinit var wakewordModel : AlexaCa2500015000100
+    private lateinit var verifierOnnx : OrtSession
 
     // Buffers
     private val newAudioData = FloatArray(1280)
@@ -55,7 +57,9 @@ class OpenWakeWord (private val context: MainActivity, private val viewModel: Ma
     private var isListening = true
     private val mediaPlayer = MediaPlayer.create(context, R.raw.ping_sound)
     private val requestPermissionLauncher: ActivityResultLauncher<String> =
-        context.registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+        context.registerForActivityResult(ActivityResultContracts.RequestPermission())
+        {
+            isGranted: Boolean ->
             if (isGranted) {
                 Toast.makeText(context, "Permission granted", Toast.LENGTH_SHORT).show()
             } else {
@@ -63,25 +67,31 @@ class OpenWakeWord (private val context: MainActivity, private val viewModel: Ma
             }
         }
 
-    // #################
-    //  --- Methods ---
-    // #################
+    // ###############################
+    //  --- Initialize services ---
+    // ###############################
 
     private fun initializeModels() {
         try {
             env = OrtEnvironment.getEnvironment()
-            val modelPath = context.assets.open("melspectrogram.onnx").readBytes()
-            melspecOnnx = env.createSession(modelPath)
+            val melspecModelPath = context.assets.open("melspectrogram.onnx").readBytes()
+            val verifierModelPath = context.assets.open("alexa_verifier.onnx").readBytes()
+
+            melspecOnnx = env.createSession(melspecModelPath)
             embeddingModel = EmbeddingModel.newInstance(context)
-            wakewordModel = Alexa20000100001200.newInstance(context)
+            wakewordModel = AlexaCa2500015000100.newInstance(context)
+            verifierOnnx = env.createSession(verifierModelPath)
+
         } catch (ex : Exception) {
             Log.e("openWakeWord", "FAILED TO LOAD MODELS")
             throw ex
         }
     }
 
-    private fun initializeMicrophone() : AudioRecord? {
-        if (ContextCompat.checkSelfPermission(context, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+    private fun initializeMicrophone() : AudioRecord?
+    {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED)
+        {
             val audioRecord = AudioRecord(
                 MediaRecorder.AudioSource.MIC,
                 16000,
@@ -93,39 +103,57 @@ class OpenWakeWord (private val context: MainActivity, private val viewModel: Ma
                 Log.e("AudioRecord", "AudioRecord initialization failed")
             }
             return audioRecord
-        } else {
+        }
+        else
+        {
             Toast.makeText(context, "Audio permission required", Toast.LENGTH_SHORT).show()
             requestPermissionLauncher.launch(Manifest.permission.RECORD_AUDIO)
             return null
         }
     }
 
-    fun startListeningForKeyword() {
+    // ###################
+    //  --- Main loop ---
+    // ###################
+
+    fun startListeningForKeyword()
+    {
         initializeModels()
         val audioRecord = initializeMicrophone()
         audioRecord?.startRecording()
         val scope = CoroutineScope(Dispatchers.IO) // Scope for background tasks
         var patience = 0
-
-        scope.launch {
-            while (isListening) {
+        var verifierScore: Float
+        scope.launch()
+        {
+            while (isListening)
+            {
                 val sizeInFloats = 1280
                 val offsetInFloats = 0
                 val floatsRead = audioRecord?.read(newAudioData, offsetInFloats, sizeInFloats, AudioRecord.READ_BLOCKING)
-                if (floatsRead == 1280) {
-
+                if (floatsRead == 1280)
+                {
                     bufferRawData()
                     bufferMelspec()
                     bufferEmbeddings()
-                    getWakeWordPrediction()
-
-                    if (patience > 0) {
+                    getWakeWordPrediction(embeddingBuffer)
+                    if (patience > 0)
+                    {
                         patience -= 1
-                    } else if (confidence[0] > 0.35 && patience == 0) {
-                        patience = maxPatience  //  Number of frames to wait until next detection
-                        withContext(Dispatchers.Main) { viewModel.addCount() }
-                        playSound()
-                        Log.d("MODEL", "Detected Keyword! Confidence $averagedConfidence Patience $patience")
+                    }
+                    else if (confidence[0] > 0.35 && patience == 0)
+                    {
+                        verifierScore = verifierOnnxPredict(embeddingBuffer)
+                        if (verifierScore > 0.35)
+                        {
+                            patience = maxPatience  //  Number of frames to wait until next detection
+                            withContext(Dispatchers.Main) { viewModel.addCount() }
+                            playSound()
+                            Log.d(
+                                "MODEL",
+                                "Detected Keyword! Confidence $averagedConfidence Patience $patience"
+                            )
+                        }
                     }
                 } else {
                     Log.e("openWakeWord", "Failed to read audio data")
@@ -134,14 +162,35 @@ class OpenWakeWord (private val context: MainActivity, private val viewModel: Ma
         }.start()
     }
 
-    private suspend fun getWakeWordPrediction() {
-        val wakewordPrediction = wakewordModelPredict(wakewordInput(embeddingBuffer))
-        confidence = wakewordPrediction.floatArray
-        addScore(confidence[0])
-        withContext(Dispatchers.Main) { viewModel.updatePredictionScore(confidence) }
+    private fun bufferRawData()
+    {
+        for (i in newAudioData.indices) {
+            newAudioData[i] = gain * newAudioData[i]
+        }
+        System.arraycopy(rawDataBuffer, 1280, rawDataBuffer, 0, 480)  // Move old data to the start
+        System.arraycopy(newAudioData, 0, rawDataBuffer, 480, 1280)  // Put new data at the end
     }
 
-    private fun bufferEmbeddings() {
+    private fun bufferMelspec()
+    {
+        val melspecPredictions = melspecOnnxPredict(rawDataBuffer)
+
+        for (i in 0 until 68) {
+            for (j in 0 until 32) {
+                melspecBuffer[0][i][j][0] = melspecBuffer[0][i + 8][j][0]
+            }
+        }
+
+        // Add the new data at the end
+        for (i in 0 until 8) {
+            for (j in 0 until 32) {
+                melspecBuffer[0][68 + i][j][0] = 2 + melspecPredictions[0][0][i][j] / 10
+            }
+        }
+    }
+
+    private fun bufferEmbeddings()
+    {
         val embeddingPredictions = embeddingModelPredict(embeddingInput(melspecBuffer))
         val newEmbeddings = embeddingPredictions.floatArray
 
@@ -158,38 +207,15 @@ class OpenWakeWord (private val context: MainActivity, private val viewModel: Ma
         }
     }
 
-    private fun bufferRawData() {
-        for (i in newAudioData.indices) {
-            newAudioData[i] = gain * newAudioData[i]
-        }
-        System.arraycopy(rawDataBuffer, 1280, rawDataBuffer, 0, 480)  // Move old data to the start
-        System.arraycopy(newAudioData, 0, rawDataBuffer, 480, 1280)  // Put new data at the end
-    }
-
-    private fun bufferMelspec() {
-        val melspecPredictions = melspecOnnxPredict(rawDataBuffer)
-        
-        for (i in 0 until 68) {
-            for (j in 0 until 32) {
-                melspecBuffer[0][i][j][0] = melspecBuffer[0][i + 8][j][0]
-            }
-        }
-
-        // Add the new data at the end
-        for (i in 0 until 8) {
-            for (j in 0 until 32) {
-                melspecBuffer[0][68 + i][j][0] = 2 + melspecPredictions[0][0][i][j] / 10
-            }
-        }
-    }
-
-    private fun melspecOnnxPredict(floatArray: FloatArray) : Array<Array<Array<FloatArray>>> {
+    private fun melspecOnnxPredict(floatArray: FloatArray) : Array<Array<Array<FloatArray>>>
+    {
         val floatBuffer = FloatBuffer.wrap(floatArray)
-        val prediction = runInference(floatBuffer)
+        val prediction = runMelspecPrediction(floatBuffer)
         return prediction!!
     }
 
-    fun runInference(inputData: FloatBuffer?): Array<Array<Array<FloatArray>>>? {
+    fun runMelspecPrediction(inputData: FloatBuffer?): Array<Array<Array<FloatArray>>>?
+    {
         try {
             val inputTensor = OnnxTensor.createTensor(env, inputData, longArrayOf(1, 1760))
             val inputs: MutableMap<String, OnnxTensor> = HashMap()
@@ -208,7 +234,8 @@ class OpenWakeWord (private val context: MainActivity, private val viewModel: Ma
         }
     }
 
-    private fun embeddingInput(data: Array<Array<Array<FloatArray>>>): ByteBuffer {
+    private fun embeddingInput(data: Array<Array<Array<FloatArray>>>): ByteBuffer
+    {
         val flattenedData = FloatArray(1 * 76 * 32 * 1)
         var index = 0
         for (i in 0 until 76) {
@@ -221,7 +248,8 @@ class OpenWakeWord (private val context: MainActivity, private val viewModel: Ma
         return byteBuffer
     }
 
-    private fun embeddingModelPredict(byteBuffer: ByteBuffer) : TensorBuffer {
+    private fun embeddingModelPredict(byteBuffer: ByteBuffer) : TensorBuffer
+    {
         val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 76, 32, 1), DataType.FLOAT32)
         inputFeature0.loadBuffer(byteBuffer)
         val outputs = embeddingModel.process(inputFeature0)
@@ -229,7 +257,8 @@ class OpenWakeWord (private val context: MainActivity, private val viewModel: Ma
         return outputFeature0
     }
 
-    private fun wakewordInput(data: Array<Array<FloatArray>>): ByteBuffer {
+    private fun wakewordInput(data: Array<Array<FloatArray>>): ByteBuffer
+    {
         val flattenedData = FloatArray(1 * 1 * 16 * 96)
         var index = 0
         for (i in 0 until 16) {
@@ -242,7 +271,8 @@ class OpenWakeWord (private val context: MainActivity, private val viewModel: Ma
         return byteBuffer
     }
 
-    private fun wakewordModelPredict(byteBuffer: ByteBuffer) : TensorBuffer {
+    private fun wakewordModelPredict(byteBuffer: ByteBuffer) : TensorBuffer
+    {
         val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 16, 96), DataType.FLOAT32)
         inputFeature0.loadBuffer(byteBuffer)
         val outputs = wakewordModel.process(inputFeature0)
@@ -250,7 +280,50 @@ class OpenWakeWord (private val context: MainActivity, private val viewModel: Ma
         return outputFeature0
     }
 
-    fun addScore(newScore: Float) {
+    private suspend fun getWakeWordPrediction(array: Array<Array<FloatArray>>)
+    {
+        val wakewordPrediction = wakewordModelPredict(wakewordInput(array))
+        confidence = wakewordPrediction.floatArray
+        withContext(Dispatchers.Main) { viewModel.updatePredictionScore(confidence) }
+        addScore(confidence[0])
+    }
+
+    private fun verifierOnnxPredict(data: Array<Array<FloatArray>>) : Float
+    {
+        val floatArray = FloatArray(1 * 1 * 16 * 96)
+        var index = 0
+        for (i in 0 until 16) {
+            for (j in 0 until 96) {
+                floatArray[index++] = data[0][i][j]
+            }
+        }
+        val floatBuffer = FloatBuffer.wrap(floatArray)
+        val prediction = runVerifierPrediction(floatBuffer)
+        return prediction!!
+    }
+
+    fun runVerifierPrediction(inputData: FloatBuffer?): Float?
+    {
+        try {
+            val inputTensor = OnnxTensor.createTensor(env, inputData, longArrayOf(1, 1536))
+            val inputs: MutableMap<String, OnnxTensor> = HashMap()
+            inputs["input"] = inputTensor
+
+            val result: OrtSession.Result = verifierOnnx.run(inputs)
+            val outputSequence = result[1] as OnnxSequence
+            val output = (outputSequence.getValue()[0].value as HashMap<*,*>)[1L] as Float
+
+            inputTensor.close()
+            outputSequence.close()
+            return output
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+            return null
+        }
+    }
+
+    fun addScore(newScore: Float)
+    {
         if (scoreQueue.size == maxScores) {
             scoreQueue.pollFirst() // Remove the oldest score
         }
@@ -258,7 +331,8 @@ class OpenWakeWord (private val context: MainActivity, private val viewModel: Ma
         averagedConfidence = scoreQueue.average().toFloat()
     }
 
-    fun playSound () {
+    fun playSound ()
+    {
         if (mediaPlayer.isPlaying) {mediaPlayer.stop(); mediaPlayer.prepare()}
         mediaPlayer.start()
     }
